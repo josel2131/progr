25fb6f4a9de0a4d334b319db1ee61053931efab8
josel2131 <josel2131@hotmail.com>
2022-11-13 16:10:11 -0500

    se sube el proyecto

---
+++ index.js
@@ -0,0 +1,18 @@
+import express  from "express"
+import cors from 'cors'
+
+
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+//app.use('/usuarios',blogRoutes)
+
+
+app.get('/',(req,res,)=>{
+    res.send('hola carevergas, seguro que son cachones')
+})
+
+app.listen(8000, ()=> {
+    console.log('Servidor Corriendo en http://localhost:8000')
+})
