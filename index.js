78c04e6b0933b80acb64ecb03f946ff53353f644
josel2131 <josel2131@hotmail.com>
2022-11-18 05:30:53 -0500

    nodemon

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-import express  from "express"
-import cors from 'cors'
+import express  from "express";
+import cors from 'cors';
+const router = express.Router();
 
 
-
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
 app.use(express.json())
-//app.use('/usuarios',blogRoutes)
+/*app.use('/usuarios',blogRoutes)*/
+app.use(router)
+router.get('/', (req,res) => {
+    res.send('bienvenidos a node js desde la raiz');
 
-
-app.get('/',(req,res,)=>{
-    res.send('hola carevergas, seguro que son cachones triplesmier erdaaa estamos en linea')
 })
+
+router.get('/login',(req,res)=>{
+    res.send('Bienvenidos carevergas');
+
+})
+
+router.post('/agregar',(req,res)=>{
+    res.send('archivo grabado con exito');
+})
+
+
+/*app.get('/',(req,res,)=>{ 
+    res.send('hola carevergas, seguro que son cachones triplesmier erdaaa estamos en linea')
+})*/
 
 app.listen(8000, ()=> {
     console.log('Servidor Corriendo en http://localhost:8000')
