1ef58b16f3ef504b38193105fd4dc1b41dfdbd2b
josel2131 <josel2131@hotmail.com>
2022-11-22 07:11:40 -0500

    package

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express  from "express";
 import cors from 'cors';
 import{ router } from './routes/rutas.js'
+import pkg from 'colors'
 
+const color = pkg;
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -11,8 +13,8 @@ app.use('/usuarios',router);
 
 /*app.get('/',(req,res,)=>{ 
     res.send('hola carevergas, seguro que son cachones triplesmier erdaaa estamos en linea')
-})*/
+})*/ 
 
 app.listen(8000, ()=> {
-    console.log('Servidor Corriendo en http://localhost:8000')
+    console.log('Servidor Corriendo en http://localhost:8000'.bgGreen)
 })
